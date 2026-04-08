8a1012a2497a5722b85d2baf174dc56b4c0fea4e
m-birke <wesci@web.de>
2026-04-08 19:47:49 +0200

    pre import

---
+++ src/mj_ext/__init__.py
@@ -0,0 +1,13 @@
+from mj_ext.mj_ext import condense_msg, get_msg, get_msg_history, get_msg_status, get_msgs_per_status, parse_msg_history
+from mj_ext.utils import init_mailjet_client, test_connection
+
+__all__ = [
+    "condense_msg",
+    "get_msg",
+    "get_msg_history",
+    "get_msg_status",
+    "get_msgs_per_status",
+    "init_mailjet_client",
+    "parse_msg_history",
+    "test_connection",
+]
